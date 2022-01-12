ba399b8579d67ef749436eeb445d05e5082d6b99
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-01-12 21:05:14 +0100

    Added new exercise to a HackRank

---
+++ Solutions/PlusMinus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    public static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+        int intResult = 0;
+        double doubleResult = 0;
+        string stringResult = String.Empty;
+
+        int inputInt = int.Parse(Console.ReadLine());
+        double inputDouble = double.Parse(Console.ReadLine());
+        string inputString = Console.ReadLine();
+
+        intResult = inputInt + i;
+        Console.WriteLine(intResult);
+
+        doubleResult = inputDouble + d ;
+        Console.WriteLine($"{doubleResult:f1}");
+
+        stringResult = s + inputString;
+        Console.WriteLine(stringResult);
+
+        
+
+        // Declare second integer, double, and String variables.
+
+        // Read and save an integer, double, and String to your variables.
+
+        // Print the sum of both integer variables on a new line.
+
+        // Print the sum of the double variables on a new line.
+
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+
+    }
+}
